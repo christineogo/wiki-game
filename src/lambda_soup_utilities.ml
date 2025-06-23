b5525f6ac54473dcd2e5f7f063de63362247c3c6
Christine Onianwa <chrisogo@mit.edu>
2025-06-23 19:33:12 +0000

    completed exercises 1-3

--- src/lambda_soup_utilities.ml
+++ src/lambda_soup_utilities.ml
@@ -34,7 +34,8 @@ let get_list_items contents : string list =
   parse contents
   $$ "li"
   |> to_list
-  |> List.map ~f:(fun li -> texts li |> String.concat ~sep:"" |> String.strip)
+  |> List.map ~f:(fun li ->
+    texts li |> String.concat ~sep:"" |> String.strip)
 ;;
 
 let%expect_test "get_list_items" =
@@ -59,21 +60,180 @@ let%expect_test "get_list_items" =
 ;;
 
 (* Gets the first item of all unordered lists contained in an HTML page. *)
+
 let get_first_item_of_all_unordered_lists contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  parse contents
+  $$ "ul"
+  |> to_list
+  |> List.map ~f:(fun ul ->
+    match Soup.select_one "li" ul with
+    | Some li -> texts li |> String.concat ~sep:"" |> String.strip
+    | None -> "")
+;;
+
+(* texts li |> String.concat ~sep:"" |> String.strip) *)
+
+let%expect_test "get_first_item_of_all_unordered_lists" =
+  (* This test specifies the HTML content directly in the file. *)
+  let contents =
+    {|<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Blog</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h2>Best Characters</h2>
+        <div class="list">
+            <ol>
+                <li><a href="Gus.html">Gus</a></li>
+                <li>Skyler</li>
+                <li>Mike</li>
+            </ol>
+        </div>
+        <div></div>
+        <img src="https://compote.slate.com/images/fb69a16d-7f35-4103-98c1-62d466196b9a.jpg?height=421&width=590">
+        <h2>Character I don't like</h2>
+        <img src="https://media.tenor.com/vGQaAfILI-oAAAAM/sad-cry.gif">
+        <div></div>
+        <div class="list">
+            <ul>
+                <li>Jesse because he cries too much</li>
+                <li>Walt because he lies to his wife</li>
+            </ul>
+        </div>
+    </body>
+    <script src="index.js"></script>
+</html>
+|}
+  in
+  List.iter (get_first_item_of_all_unordered_lists contents) ~f:print_endline;
+  [%expect
+    {|
+    Jesse because he cries too much
+    |}]
 ;;
 
 (* Gets the first item of the second unordered list in an HTML page. *)
 let get_first_item_of_second_unordered_list contents : string =
-  ignore (contents : string);
-  failwith "TODO"
+  match get_first_item_of_all_unordered_lists contents with
+  | [] -> ""
+  | _ :: [] -> ""
+  | _ :: _ ->
+    List.hd_exn
+      (List.tl_exn (get_first_item_of_all_unordered_lists contents))
+;;
+
+let%expect_test "get_first_item_of_second_unordered_list" =
+  (* This test specifies the HTML content directly in the file. *)
+  let contents =
+    {|<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Blog</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h2>Best Characters</h2>
+        <div class="list">
+            <ol>
+                <li><a href="Gus.html">Gus</a></li>
+                <li>Skyler</li>
+                <li>Mike</li>
+            </ol>
+        </div>
+        <div></div>
+        <img src="https://compote.slate.com/images/fb69a16d-7f35-4103-98c1-62d466196b9a.jpg?height=421&width=590">
+        <h2>Character I don't like</h2>
+        <img src="https://media.tenor.com/vGQaAfILI-oAAAAM/sad-cry.gif">
+        <div></div>
+        <div class="list">
+            <ul>
+                <li>Jesse because he cries too much</li>
+                <li>Walt because he lies to his wife</li>
+            </ul>
+        </div>
+    </body>
+    <script src="index.js"></script>
+</html>
+|}
+  in
+  print_endline (get_first_item_of_second_unordered_list contents);
+  [%expect
+    {|
+    |}]
 ;;
 
 (* Gets all bolded text from an HTML page. *)
 let get_bolded_text contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  let strong_words =
+    parse contents
+    $$ "strong"
+    |> to_list
+    |> List.map ~f:(fun li ->
+      texts li |> String.concat ~sep:"" |> String.strip)
+  in
+  let bold_words =
+    parse contents
+    $$ "b"
+    |> to_list
+    |> List.map ~f:(fun li ->
+      texts li |> String.concat ~sep:"" |> String.strip)
+  in
+  List.concat [ bold_words; strong_words ]
+;;
+
+let%expect_test "get_bolded_text" =
+  (* This test specifies the HTML content directly in the file. *)
+  let contents =
+    {|<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Blog</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h2>Best Characters</h2>
+        <div class="list">
+            <ol>
+                <li><a href="Gus.html">Gus</a></li>
+                <li>Skyler</li>
+                <li>Mike</li>
+            </ol>
+        </div>
+        <div></div>
+        <img src="https://compote.slate.com/images/fb69a16d-7f35-4103-98c1-62d466196b9a.jpg?height=421&width=590">
+        <h2>Character I don't like</h2>
+        <img src="https://media.tenor.com/vGQaAfILI-oAAAAM/sad-cry.gif">
+        <div></div>
+        <div class="list">
+            <ul>
+                <li><b>Jesse because he cries too much</b></li>
+                <li><strong>Walt because he lies to his wife</strong></li>
+            </ul>
+        </div>
+    </body>
+    <script src="index.js"></script>
+</html>
+|}
+  in
+  List.iter (get_bolded_text contents) ~f:print_endline;
+  [%expect
+    {|
+    Jesse because he cries too much
+    Walt because he lies to his wife
+    |}]
 ;;
 
 (* [make_command ~summary ~f] is a helper function that builds a simple HTML parsing
@@ -92,12 +252,15 @@ let make_command ~summary ~f =
 ;;
 
 let print_title_command =
-  make_command ~summary:"print the title from an HTML page" ~f:(fun contents ->
-    [ get_title contents ])
+  make_command
+    ~summary:"print the title from an HTML page"
+    ~f:(fun contents -> [ get_title contents ])
 ;;
 
 let print_list_items_command =
-  make_command ~summary:"print all list items from an HTML page" ~f:get_list_items
+  make_command
+    ~summary:"print all list items from an HTML page"
+    ~f:get_list_items
 ;;
 
 let print_first_item_of_all_unordered_lists_command =
@@ -113,7 +276,9 @@ let print_first_item_of_second_unordered_list_command =
 ;;
 
 let print_bolded_text_command =
-  make_command ~summary:"print all bolded text in an HTML page" ~f:get_bolded_text
+  make_command
+    ~summary:"print all bolded text in an HTML page"
+    ~f:get_bolded_text
 ;;
 
 let command =
