1478ba8bd4fcac382bfe5e0c9e0fddad499a71b6
Christine Onianwa <chrisogo@mit.edu>
2025-06-23 20:58:57 +0000

    finished wiki_game

--- src/wiki_game.ml
+++ src/wiki_game.ml
@@ -15,8 +15,42 @@ open! Core
    uniformity in article format. We can expect that all Wikipedia article links parsed
    from a Wikipedia page will have the form "/wiki/<TITLE>". *)
 let get_linked_articles contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  (* plan: get all links, filter to ones that sart with en.wikipedia.org, filter to no namespaces *)
+  let open Soup in
+  let is_wiki link = String.sub link ~pos:0 ~len:5 in
+  parse contents
+  $$ "a"
+  |> to_list
+  |> List.filter ~f:(fun a -> String.length (R.attribute "href" a) > 4)
+  |> List.map ~f:(fun a ->
+    match is_wiki (R.attribute "href" a) with
+    | "/wiki" -> R.attribute "href" a
+    | _ -> " ")
+  |> List.filter ~f:(fun a -> match a with " " -> false | _ -> true)
+  |> List.filter ~f:(fun a ->
+    match Wikipedia_namespace.namespace a with None -> true | _ -> false)
+  |> List.dedup_and_sort ~compare:String.compare
+;;
+
+let%expect_test "get_linked_articles" =
+  (* This test uses existing files on the filesystem. *)
+  let contents =
+    File_fetcher.fetch_exn
+      (Local (File_path.of_string "../resources/wiki"))
+      ~resource:"Carnivore"
+  in
+  List.iter (get_linked_articles contents) ~f:print_endline;
+  [%expect
+    {|
+    All feliforms, such as domestic cats, big cats, hyenas, mongooses, civets
+    Almost all caniforms, such as the dogs, wolves, foxes, ferrets, seals and walruses
+    All cetaceans, such as dolphins, whales and porpoises
+    All bats except fruitbats
+    The carnivorous marsupials, such as the Tasmania devil
+    All birds of prey, such as hawks, eagles, falcons and owls
+    All vultures, both old world and new
+    Most waterfowl, such as gulls, penguins, pelicans, storks, and herons
+    |}]
 ;;
 
 let print_links_command =
@@ -47,8 +81,8 @@ let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let how_to_fetch = File_fetcher.How_to_fetch.param
       and origin = flag "origin" (required string) ~doc:" the starting page"
@@ -87,11 +121,14 @@ let find_path ?(max_depth = 3) ~origin ~destination ~how_to_fetch () =
 let find_path_command =
   let open Command.Let_syntax in
   Command.basic
-    ~summary:"Play wiki game by finding a link between the origin and destination pages"
+    ~summary:
+      "Play wiki game by finding a link between the origin and destination \
+       pages"
     [%map_open
       let how_to_fetch = File_fetcher.How_to_fetch.param
       and origin = flag "origin" (required string) ~doc:" the starting page"
-      and destination = flag "destination" (required string) ~doc:" the destination page"
+      and destination =
+        flag "destination" (required string) ~doc:" the destination page"
       and max_depth =
         flag
           "max-depth"
